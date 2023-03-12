428aeffb97e1affe87e2e0af38e0ec5c106f8c50
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-03-12 16:24:42 +0530

    update the header & page not found pages

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import {FaDonate} from 'react-icons/fa';
 
 const Header = () => {
   return (
     <>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <NavLink to="/" className="navbar-brand" href="#">Navbar</NavLink>
+    <Link to="/" className="navbar-brand"><FaDonate></FaDonate> Donation site</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
+          <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
+          <NavLink to="/category" className="nav-link" aria-current="page">Category</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
+          <NavLink to="/register" className="nav-link">Register</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/save" className="nav-link" href="#">Save (0)</NavLink>
+          <NavLink to="/login" className="nav-link">Login</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/save" className="nav-link">Save (0)</NavLink>
         </li>
       </ul>
       
