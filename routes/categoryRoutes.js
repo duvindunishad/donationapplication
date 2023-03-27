37d98ef60c6fde3d6498d58c893a1f54932585e8
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-03-27 17:03:08 +0530

    UPDATE/DELETE/CREATE CATEGORY

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,16 +1,40 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddlewares.js";
-import { createCategoryController } from "../controllers/categoryController.js";
+import {
+  categoryController,
+  createCategoryController,
+  deleteCategoryCOntroller,
+  singleCategoryController,
+  updateCategoryController,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
-//routes
+// routes for category routes
 
+//create category routes
 router.post(
   "/create-category",
   requireSignIn,
   isAdmin,
   createCategoryController
 );
+
+//update category routes
+router.put(
+  "/update-category/:id", //here pass the category id to update category
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
+
+//get all category routes
+router.get("/get-category", categoryController);
+
+//get single category routes
+router.get("/single-category/:slug", singleCategoryController); //replace slug as id
+
+//delete category routes
+router.delete("/delete-category/:id", deleteCategoryCOntroller); //replace slug as id
 
 export default router;
