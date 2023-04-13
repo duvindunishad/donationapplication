f29b6dfa22234c71355dc1b6617e7602ee814eff
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-13 14:09:23 +0530

    update order.js page front end view

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,6 +5,7 @@ import {
   testController,
   forgotPasswordController,
   updateProfileController,
+  getOrdersController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddlewares.js";
 
@@ -39,7 +40,7 @@ router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
 router.put("/profile", requireSignIn, updateProfileController);
 
 // //orders
-// router.get("/orders", requireSignIn, getOrdersController);
+router.get("/orders", requireSignIn, getOrdersController);
 
 // //all orders
 // router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
