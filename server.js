fc760538dce3f5cb120a034d613f80e14eedb0b7
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-22 02:08:38 +0530

    ww

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/api/v1/user", userRoute);
 //   // res.sendFile(path.join(__dirname, "./client/build/index.html"));
 //   res.sendFile(path.join(__dirname, "./var/task/client/build/index.html"));
 // });
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
