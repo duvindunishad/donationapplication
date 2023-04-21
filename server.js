624fb3aefeb9db088274636c34a38afd99ac0669
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-21 16:39:27 +0530

    h

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use("/api/v1/user", userRoute);
 // });
 //rest api
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, ".client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //port, here add 8080 port , if their any erro from the env file ,but port will work perfect
