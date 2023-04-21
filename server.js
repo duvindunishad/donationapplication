250221c188ee0ce2fcd8258f57f5bfcb543f8ed9
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-21 16:24:44 +0530

    pp

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -42,12 +42,12 @@ app.use("/api/v1/user", userRoute);
 //   res.send("<h1>welcome to donation application</h1>");
 // });
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 //port, here add 8080 port , if their any erro from the env file ,but port will work perfect
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 //run list
 
