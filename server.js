11ae23c56ea0358ddfbfd877ab7b67cc6f421c63
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-22 00:18:46 +0530

    ws

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, "./client/build")));
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "/var/task/client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -50,7 +50,7 @@ app.use("/api/v1/user", userRoute);
 
 app.use("*", function (req, res) {
   // res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  res.sendFile(path.join("/var/task/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./var/task/client/build/index.html"));
 });
 
 //port, here add 8080 port , if their any erro from the env file ,but port will work perfect
