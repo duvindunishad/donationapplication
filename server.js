dbc29918704ba0ddec9cac4c3816d3f41005ab00
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-22 13:12:44 +0530

    server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
 //configure the env
 dotenv.config();
@@ -30,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, "./client/build")));
-app.use(express.static(path.join(__dirname, ".../client/build")));
+app.use(express.static(path.join(__dirname, "")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
