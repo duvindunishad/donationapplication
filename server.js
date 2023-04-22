d1a5639f55177dd75eea8c1f743d096ff06fba1a
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-22 20:42:08 +0530

    comment updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dotenv.config();
 //database config
 connectDB();
 
-//esmodule fix
+//es-module fix
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -30,7 +30,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
@@ -41,22 +40,19 @@ app.use("/api/v1/user", userRoute);
 // app.use("/api/v1/users", userRoute);
 
 //old
-//rest api
+//rest api need to active when deploy offline localhost
 
 // app.get("/", (req, res) => {
 //   res.send("<h1><u>welcome to donation application</u></h1>");
 // });
 
-//rest api
+//rest api need to active when deploy online
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"), function (err) {
     res.status(500).send(err);
   });
 });
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 //port, here add 8080 port , if their any erro from the env file ,but port will work perfect
 const PORT = process.env.PORT || 8080;
