a06cb9b3e8bc0943e06d8dbb498f81bb170bf0c3
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2023-04-22 14:55:54 +0530

    hh

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, "./client/build")));
-app.use(express.static(path.join(__dirname, "./client/public")));
+app.use(express.static(path.join(__dirname, "./client")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
